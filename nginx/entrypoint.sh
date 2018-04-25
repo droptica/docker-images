0ac00273783fd1772bfe139b54638956ec1b4dd0
Daniel Kossakowski <daniel.kossakowski@droptica.pl>
2018-04-25 15:41:06 +0200

    Move nginx to separated repository

--- nginx/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$WEB_USER" ] && [ "$WEB_PASS" ]; then
-    printf "${WEB_USER}:`openssl passwd -apr1 $WEB_PASS`\n" >> /etc/nginx/.htpasswd
-    cp /etc/nginx/passwd.conf /etc/nginx/conf.d/passwd.conf
-fi
-
-exec nginx -g "daemon off;"
